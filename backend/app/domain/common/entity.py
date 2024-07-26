3e2b1e01a1a9f903b237c313194cbbcc0a59b10e
Trinadtsatiy <testiiing44679@gmail.com>
2024-07-26 19:10:39 +0300

    entity

---
+++ backend/app/domain/common/entity.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+from backend.app.domain.common.value_object import ValueObject
+
+
+EntityId = TypeVar("EntityId", bound=ValueObject)
+
+
+@dataclass
+class Entity(Generic[EntityId]):
+    id: EntityId
---
+++ backend/app/domain/common/error.py
@@ -0,0 +1,8 @@
+class DomainError(Exception):
+    def __init__(self, message: str, *args: object) -> None:
+        self.message = message
+        super().__init__(message, *args)
+
+
+class DomainValidationError(DomainError):
+    pass
